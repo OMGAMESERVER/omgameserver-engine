03d87fd2d12617a7f6fbf1fbe6e1944cffe6f300
Kirill Byvshev <k@byv.sh>
2020-07-05 21:30:14 +0300

    input service testing

--- src/test/java/com/omgameserver/engine/BaseServiceTest.java
+++ src/test/java/com/omgameserver/engine/BaseServiceTest.java
@@ -1,14 +1,18 @@
 package com.omgameserver.engine;
 
 import com.crionuke.bolts.Dispatcher;
+import com.omgameserver.engine.events.IncomingDatagramEvent;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 
-public class BaseServiceTest extends Assert {
+public class BaseServiceTest extends Assert implements OmgsConstants {
     static private final Logger logger = LoggerFactory.getLogger(BaseServiceTest.class);
 
     protected final int POLL_TIMEOUT_MS = 1000;
@@ -18,8 +22,8 @@ public class BaseServiceTest extends Assert {
     protected final int PROPERTY_THREAD_POOL_SIZE = 32;
     protected final int PROPERTY_QUEUE_SIZE = 128;
     protected final int PROPERTY_TICK_INTERVAL = 100;
-    protected final int PROPERTY_DISCONNECT_INTERVAL = 5000;
-    protected final int PROPERTY_PING_INTERVAL = 1000;
+    protected final int PROPERTY_DISCONNECT_INTERVAL = 1000;
+    protected final int PROPERTY_PING_INTERVAL = 250;
     protected final String PROPERTY_MAIN_SCRIPT = "main.lua";
 
     protected OmgsProperties properties;
@@ -35,5 +39,43 @@ public class BaseServiceTest extends Assert {
         threadPoolTaskExecutor.setCorePoolSize(properties.getThreadPoolSize());
         threadPoolTaskExecutor.initialize();
         logger.info("Thread pool with size={} created", properties.getThreadPoolSize());
+    }
+
+    protected SocketAddress generateSocketAddress() {
+        SocketAddress socketAddress = new InetSocketAddress("0.0.0.0", (int) (Math.random() * 55535) + 10000);
+        logger.info("Generated socketAddress={}", socketAddress);
+        return socketAddress;
+    }
+
+    protected Long generateClientUid() {
+        Long clientUid = Long.valueOf((int) (Math.random() * 89999 + 10000));
+        logger.info("Generated clientUid={}", clientUid);
+        return clientUid;
+    }
+
+    protected IncomingDatagramEvent createIncomingDatagramEvent(SocketAddress sourceAddress, int seq, int ack, int bit,
+                                                                byte sys, String payload) {
+        ByteBuffer datagram = ByteBuffer.allocate(BUFFER_SIZE);
+        datagram.putInt(seq);
+        datagram.putInt(ack);
+        datagram.putInt(bit);
+        datagram.put(sys);
+        datagram.put(payload.getBytes());
+        datagram.flip();
+        return new IncomingDatagramEvent(sourceAddress, datagram);
+    }
+
+    protected ByteBuffer skipHeader(ByteBuffer byteBuffer) {
+        byteBuffer.getInt();
+        byteBuffer.getInt();
+        byteBuffer.getInt();
+        byteBuffer.get();
+        return byteBuffer;
+    }
+
+    protected String readPayload(ByteBuffer byteBuffer) {
+        byte[] bytes = new byte[byteBuffer.remaining()];
+        byteBuffer.get(bytes);
+        return new String(bytes);
     }
 }
---
+++ src/test/java/com/omgameserver/engine/networking/InputServiceTest.java
@@ -0,0 +1,152 @@
+package com.omgameserver.engine.networking;
+
+import com.crionuke.bolts.Bolt;
+import com.omgameserver.engine.BaseServiceTest;
+import com.omgameserver.engine.events.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class InputServiceTest extends BaseServiceTest {
+    static private final Logger logger = LoggerFactory.getLogger(InputServiceTest.class);
+
+    private InputService inputService;
+    private ConsumerStub consumerStub;
+
+    private BlockingQueue<IncomingHeaderEvent> incomingHeaderEvents;
+    private BlockingQueue<IncomingPayloadEvent> incomingPayloadEvents;
+    private BlockingQueue<ClientDisconnectedEvent> clientDisconnectedEvents;
+
+    @Before
+    public void beforeTest() throws IOException {
+        createComponents();
+        inputService = new InputService(properties, threadPoolTaskExecutor, dispatcher);
+        inputService.postConstruct();
+        consumerStub = new ConsumerStub();
+        consumerStub.postConstruct();
+        incomingHeaderEvents = new LinkedBlockingQueue<>(PROPERTY_QUEUE_SIZE);
+        incomingPayloadEvents = new LinkedBlockingQueue<>(PROPERTY_QUEUE_SIZE);
+        clientDisconnectedEvents = new LinkedBlockingQueue<>(PROPERTY_QUEUE_SIZE);
+    }
+
+    @After
+    public void afterTest() {
+        consumerStub.finish();
+        inputService.finish();
+    }
+
+    @Test
+    public void testSplitHeaderAndPayload() throws InterruptedException {
+        // Send datagram with specified source address and header
+        SocketAddress sourceAddress = generateSocketAddress();
+        dispatcher.dispatch(createIncomingDatagramEvent(sourceAddress,
+                1, 2,3, (byte) 0,"payload"));
+        // Waiting header event
+        IncomingHeaderEvent incomingHeaderEvent = incomingHeaderEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        logger.info("Got {}", incomingHeaderEvent);
+        // Checks
+        assertNotNull(incomingHeaderEvent);
+        assertTrue(incomingHeaderEvent.getSocketAddress() == sourceAddress);
+        assertTrue(incomingHeaderEvent.getSeq() == 1);
+        assertTrue(incomingHeaderEvent.getAck() == 2);
+        assertTrue(incomingHeaderEvent.getBit() == 3);
+        assertTrue(incomingHeaderEvent.getSys() == 0);
+    }
+
+    @Test
+    public void testPayloadSplit() throws InterruptedException {
+        // Send datagram with specified payload
+        String testPayload = "payload";
+        dispatcher.dispatch(createIncomingDatagramEvent(generateSocketAddress(),
+                1, 2,3, (byte) 0, testPayload));
+        // Waiting for payload event
+        IncomingPayloadEvent incomingPayloadEvent = incomingPayloadEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        logger.info("Got {}", incomingPayloadEvent);
+        // Asserts
+        assertNotNull(incomingPayloadEvent);
+        String incomingPayload = readPayload(incomingPayloadEvent.getPayload());
+        assertEquals(testPayload, incomingPayload);
+    }
+
+    @Test
+    public void testDisconnectInterval() throws InterruptedException {
+        // Send datagram to client creation
+        SocketAddress sourceAddress = generateSocketAddress();
+        dispatcher.dispatch(createIncomingDatagramEvent(sourceAddress,
+                1, 0 ,0, (byte) 0, "payload"));
+        // InputService check disconnect interval for clients every tick
+        Thread.sleep(properties.getDisconnectInterval() * 2);
+        dispatcher.dispatch(new TickEvent(1,PROPERTY_DISCONNECT_INTERVAL * 2));
+        // Waiting for disconnect event
+        ClientDisconnectedEvent clientDisconnectedEvent =
+                clientDisconnectedEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        logger.info("Got {}", clientDisconnectedEvent);
+        // Asserts
+        assertNotNull(clientDisconnectedEvent);
+        assertTrue(clientDisconnectedEvent.getSocketAddress() == sourceAddress);
+    }
+
+    @Test
+    public void testDisconnectRequest() throws InterruptedException {
+        // Send datagram to client creation
+        SocketAddress sourceAddress = generateSocketAddress();
+        dispatcher.dispatch(createIncomingDatagramEvent(sourceAddress,
+                1, 0 ,0, (byte) 0, "payload"));
+        // Waiting for payload event with clientUid
+        IncomingPayloadEvent payloadEvent = incomingPayloadEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        logger.info("Got {}", payloadEvent);
+        assertNotNull(payloadEvent);
+        // Send disconnect call for clientUid
+        Long clientUid = payloadEvent.getClientUid();
+        logger.info("Send disconnect call for clientUid={}", clientUid);
+        dispatcher.dispatch(new DisconnectClientRequestEvent(clientUid));
+        // Waiting for disconnection event
+        ClientDisconnectedEvent clientDisconnectedEvent =
+                clientDisconnectedEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        logger.info("Got {}", clientDisconnectedEvent);
+        // Asserts
+        assertNotNull(clientDisconnectedEvent);
+        assertTrue(clientDisconnectedEvent.getSocketAddress() == sourceAddress);
+    }
+
+    private class ConsumerStub extends Bolt implements
+            IncomingHeaderEvent.Handler,
+            IncomingPayloadEvent.Handler,
+            ClientDisconnectedEvent.Handler {
+        private final Logger logger = LoggerFactory.getLogger(ConsumerStub.class);
+
+        public ConsumerStub() {
+            super("consumer-stub", PROPERTY_QUEUE_SIZE);
+        }
+
+        @Override
+        public void handleIncomingHeader(IncomingHeaderEvent event) throws InterruptedException {
+            incomingHeaderEvents.put(event);
+        }
+
+        @Override
+        public void handleIncomingPayload(IncomingPayloadEvent event) throws InterruptedException {
+            incomingPayloadEvents.put(event);
+        }
+
+        @Override
+        public void handleClientDisconnected(ClientDisconnectedEvent event) throws InterruptedException {
+            clientDisconnectedEvents.put(event);
+        }
+
+        public void postConstruct() {
+            threadPoolTaskExecutor.execute(this);
+            dispatcher.subscribe(this, IncomingHeaderEvent.class);
+            dispatcher.subscribe(this, IncomingPayloadEvent.class);
+            dispatcher.subscribe(this, ClientDisconnectedEvent.class);
+        }
+    }
+}
