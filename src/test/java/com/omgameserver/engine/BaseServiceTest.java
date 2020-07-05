17af5e13a130130987747eb4966968abc6a524c2
Kirill Byvshev <k@byv.sh>
2020-07-05 12:55:39 +0300

    base class for testing and test for tick service

---
+++ src/test/java/com/omgameserver/engine/BaseServiceTest.java
@@ -0,0 +1,41 @@
+package com.omgameserver.engine;
+
+import com.crionuke.bolts.Dispatcher;
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.net.UnknownHostException;
+
+public class BaseServiceTest extends Assert {
+    static private final Logger logger = LoggerFactory.getLogger(BaseServiceTest.class);
+
+    protected final int POLL_TIMEOUT_MS = 1000;
+
+    protected final String PROPERTY_HOST = "0.0.0.0";
+    protected final int PROPERTY_PORT = 12345;
+    protected final int PROPERTY_THREAD_POOL_SIZE = 32;
+    protected final int PROPERTY_QUEUE_SIZE = 128;
+    protected final int PROPERTY_TICK_INTERVAL = 100;
+    protected final int PROPERTY_DISCONNECT_INTERVAL = 5000;
+    protected final int PROPERTY_PING_INTERVAL = 1000;
+    protected final int PROPERTY_DATAGRAM_MAX_SIZE = 1024;
+    protected final String PROPERTY_MAIN_SCRIPT = "main.lua";
+
+    protected OmgsProperties properties;
+    protected Dispatcher dispatcher;
+    protected ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    protected void createComponents() throws UnknownHostException {
+        properties = new OmgsProperties(PROPERTY_HOST, PROPERTY_PORT, PROPERTY_THREAD_POOL_SIZE, PROPERTY_QUEUE_SIZE,
+                PROPERTY_TICK_INTERVAL, PROPERTY_DISCONNECT_INTERVAL, PROPERTY_PING_INTERVAL,
+                PROPERTY_DATAGRAM_MAX_SIZE, PROPERTY_MAIN_SCRIPT);
+        dispatcher = new Dispatcher();
+        threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("test-");
+        threadPoolTaskExecutor.setCorePoolSize(properties.getThreadPoolSize());
+        threadPoolTaskExecutor.initialize();
+        logger.info("Thread pool with size={} created", properties.getThreadPoolSize());
+    }
+}
---
+++ src/test/java/com/omgameserver/engine/utils/TickServiceTest.java
@@ -0,0 +1,69 @@
+package com.omgameserver.engine.utils;
+
+import com.crionuke.bolts.Bolt;
+import com.omgameserver.engine.BaseServiceTest;
+import com.omgameserver.engine.events.TickEvent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.UnknownHostException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TickServiceTest extends BaseServiceTest {
+    static private final Logger logger = LoggerFactory.getLogger(TickServiceTest.class);
+
+    TickService tickService;
+    BlockingQueue<TickEvent> tickEvents;
+    ConsumerStub consumerStub;
+
+    @Before
+    public void beforeTest() throws UnknownHostException {
+        createComponents();
+        tickService = new TickService(properties, dispatcher, threadPoolTaskExecutor);
+        tickService.postConstruct();
+        tickEvents = new LinkedBlockingQueue<>(PROPERTY_QUEUE_SIZE);
+        consumerStub = new ConsumerStub();
+        consumerStub.postConstruct();
+    }
+
+    @After
+    public void afterTest() {
+        tickService.finish();
+        consumerStub.finish();
+    }
+
+    @Test
+    public void testTickEvents() throws InterruptedException {
+        TickEvent tick1 = tickEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        TickEvent tick2 = tickEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        TickEvent tick3 = tickEvents.poll(POLL_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+        assertNotNull(tick1);
+        assertEquals(tick1.getNumber(), 1);
+        assertNotNull(tick2);
+        assertEquals(tick2.getNumber(), 2);
+        assertNotNull(tick3);
+        assertEquals(tick3.getNumber(), 3);
+    }
+
+    private class ConsumerStub extends Bolt implements TickEvent.Handler {
+
+        ConsumerStub() {
+            super("consumer-stub", PROPERTY_QUEUE_SIZE);
+        }
+
+        @Override
+        public void handleTick(TickEvent event) throws InterruptedException {
+            tickEvents.put(event);
+        }
+
+        void postConstruct() {
+            threadPoolTaskExecutor.execute(this);
+            dispatcher.subscribe(this, TickEvent.class);
+        }
+    }
+}
